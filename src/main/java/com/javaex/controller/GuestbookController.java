3a626a78ae1f46e3f6509eaea202d50146ce2dd0
ds-2024 <yoonds714@nate.com>
2024-04-07 23:43:37 +0900

    수정추가

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -3,24 +3,43 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
 import com.javaex.vo.GuestbookVo;
 
-@Controller
+//@Controller
+@RestController
 public class GuestbookController {
 	
 	@Autowired
 	private GuestbookService guestbookService;
 	
+	//삭제
+	//@ResponseBody
+	@DeleteMapping(value="/api/guests/{no}")
+	public String remove(@RequestBody GuestbookVo guestbookVo, 
+						 @PathVariable(value="no") int no) { //{no}는 동적으로 변하는 값 => PathVariable
+		System.out.println("GuestbookController.remove()");
+		
+		guestbookVo.setNo(no);
+		System.out.println(guestbookVo);
+		
+		int count = guestbookService.exeRemove(guestbookVo);
+		
+		String result = "{\"count\": "+ count +"}";
+		System.out.println(result);
+		return result;
+	}
+	
 	//저장 + 1개 가져오기
-	@ResponseBody
-	@RequestMapping(value="/api/guests", method = RequestMethod.POST)
+	//@ResponseBody
+	@PostMapping(value="/api/guests")
 	public GuestbookVo add(@RequestBody GuestbookVo guestbookVo) {
 		System.out.println("GuestbookController.add()"); 
 		System.out.println(guestbookVo);
@@ -31,8 +50,8 @@ public class GuestbookController {
 	}
 	
 	//리스트
-	@ResponseBody
-	@RequestMapping(value="/api/guests", method = RequestMethod.GET)
+	//@ResponseBody
+	@GetMapping(value="/api/guests")
 	public List<GuestbookVo> list() {
 		System.out.println("GuestbookController.list()");
 		
